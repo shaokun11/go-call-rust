f00a8d39c977c3c1513a09b19b1389f6cf03b4aa
shaokun <605416017@qq.com>
2023-04-23 12:11:15 +0800

    完成测试go和rust互相通信

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,12 +1,24 @@
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
+use std::{ptr, slice};
+
+extern "C" {
+    fn EthSayGo();
+    fn EthSayGoHello() -> *const c_char;
+}
 
 #[no_mangle]
 pub extern "C" fn hello(name: *const libc::c_char) -> *const c_char {
     let name_cstr = unsafe { CStr::from_ptr(name) };
     let name = name_cstr.to_str().unwrap();
-    let say = format!("Hello {}!", name);
-    println!("rust say {}", say.clone());
+    // call go
+    unsafe { EthSayGo() }
+
+    let hello_cstr = unsafe { CStr::from_ptr(EthSayGoHello()) };
+    let hello_str = hello_cstr.to_str().unwrap();
+    println!("-----------{}------", hello_str);
+    // return to go
+    let say = format!("rust 3 Hello {}!", name);
     let say_cstr = CString::new(say).unwrap();
     say_cstr.into_raw()
 }
--- main.go
+++ main.go
@@ -3,17 +3,27 @@ package main
 // NOTE: There should be NO space between the comments and the `import "C"` line.
 
 /*
-#cgo LDFLAGS: -L./lib -lhello
-#include "./hello/hello.h"
+#cgo LDFLAGS: ./lib/libhello.a -ldl
+#include "hello.h"
 #include <stdlib.h>
 */
 import "C"
 import "unsafe"
 
+//export EthSayGo
+func EthSayGo() {
+	println("rust will call go 1 ")
+}
+
+//export EthSayGoHello
+func EthSayGoHello() *C.char {
+	return C.CString("Hello from Go! 1 ")
+}
+
 func main() {
-	var name = C.CString("world")
+	var name = C.CString("world 1 ")
 	defer C.free(unsafe.Pointer(name))
 	var str = C.hello(name)
 	defer C.free(unsafe.Pointer(str))
-	println("go say :", C.GoString(str))
+	println("go call rust result :", C.GoString(str))
 }
